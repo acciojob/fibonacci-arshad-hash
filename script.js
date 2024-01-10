37492efe4594c0c9d0b347077a742a8b424ff5a0
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2024-01-10 14:53:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function fibonacci(num) {
 // your code here
-	if (num === 1) {
-        return 0;
-    } else if (num === 2) {
-        return 1;
-    } else {
-        return fibonacci(num - 1) + fibonacci(num - 2);
-    }
+	if (num == 1)
+        return 0;
+    if (num == 2)
+        return 1;
+    let num1 = 0;
+    let num2 = 1;
+    let sum;
+    let i = 2;
+    while (i < num) {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+        i += 1;
+    }
+    return num2;
 }
 
 module.exports = fibonacci;
